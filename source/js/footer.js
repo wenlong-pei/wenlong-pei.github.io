f46d9988573809c29d6b0f9676016f2eef5c68c6
Wenlong Pei <2668187831@qq.com>
2023-09-05 13:37:18 +0800

    change1

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;">© 2021-2022 TuHome</p>\n' +
+    '<p style="color:#333;">© 2023 All rights reserved by <a href="https://github.com/wenlong-pei">Wenlong Pei</a></p>\n' +
     '</center>\n' +
     '');
 
