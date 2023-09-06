13e4a1dd27edb375c5cef33b6d6fe3aef199f9c4
Wenlong Pei <2668187831@qq.com>
2023-09-06 09:24:39 +0800

    more change

--- source/js/header.js
+++ source/js/header.js
@@ -2,8 +2,8 @@ document.write('' +
     '<link rel="stylesheet" href="https://jsd.licaoz.com/gh/bynotes/texiao/source/css/shubiao.css">\n' +
     '<link rel="stylesheet" href="https://at.alicdn.com/t/font_2037988_gjhbcpzhs67.css" type="text/css">\n' +
 //     '<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?8c0854c37b23b617c441acb65529fca1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>\n' +
-    '<link rel="apple-touch-icon" sizes="76x76" href="source/img/logo.png">\n' +
-    '<link rel="shortcut icon" href="source/img/logo.png" type="image/x-png">\n' +
+    '<link rel="apple-touch-icon" sizes="76x76" href="source/img/picture.jpg">\n' +
+    '<link rel="shortcut icon" href="source/img/picture.jpg" type="image/x-png">\n' +
     '<link rel="stylesheet" href="source/css/mystyle.css">\n' +
     '');
 
