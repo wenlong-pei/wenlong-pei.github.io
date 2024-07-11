07208425e54644f3cc7abe8df3b8b2e110a9a3f6
Wenlong Pei <2668187831@qq.com>
2024-07-11 13:30:28 +0800

    lll

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;">© 2023 All rights reserved by <a href="https://github.com/wenlong-pei">Wenlong Pei</a></p>\n' +
+    '<p style="color:#333;">© 2024 All rights reserved by <a href="https://github.com/wenlong-pei">Wenlong Pei</a></p>\n' +
     '</center>\n' +
     '');
 
